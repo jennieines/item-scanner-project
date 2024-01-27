58fb45a32aa5ef9d0bd02372de40946200de36f5
jennieines <141196314+jennieines@users.noreply.github.com>
2024-01-27 12:18:57 -0500

    created db-setup.sql

---
+++ server/db-setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE itemscanner;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(30) NOT NULL
+);
+
+TABLE users;
+ --display the users table and info
+
+ INSERT INTO users (username, password) VALUES ('test_user', 'test_password');
